e477f6e32089783277fd04331242bb622cf9cd0e
Harith Khairolanuar <33217725+harithkhairol@users.noreply.github.com>
2019-01-09 14:01:15 +0800

    Update google-config.php

--- google-config.php
+++ google-config.php
@@ -4,8 +4,8 @@ session_start();
 require_once "GoogleAPI/vendor/autoload.php";
 
 $gClient = new Google_Client();
-$gClient->setClientId("739291216997-kt32grltkch5du6kkmhe6j7mj28788j1.apps.googleusercontent.com");
-$gClient->setClientSecret("g5w7mReLo6Rbdmpt-WQBsdiG");
+$gClient->setClientId("YOUR CLIENT ID");
+$gClient->setClientSecret("YOUR CLIENT SECRET");
 $gClient->setApplicationName("Myezgo Login Tutorial");
 $gClient->setRedirectUri("http://localhost/google_study/google-callback.php");
 $gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
